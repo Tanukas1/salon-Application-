64c6ac1a40664e5bd286935ea9241e5efd7ea71f
tanukas1 <tanukashyap2019@gmail.com>
2024-04-08 17:35:21 +0530

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Loader.css'
+export default function Loader() {
+  return (
+   <div className="container m-5">
+      <div className="row">
+        <div className="col-lg-4 col-md-6  mb-4">
+          <div className="card shadow-lg bg-white" style={{ height: "43rem", borderRadius: 50 }}>
+            <div className="container">
+            
+            
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
